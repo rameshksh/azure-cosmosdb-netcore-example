02e83040ad5b309259a2b9076d09710331cc2ff5
rameshksh <ramesh.sharma12@hotmail.com>
2020-04-07 09:12:04 +0530

    Demo project

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryForDocument().Wait();
+        }
+
+        private async static Task QueryForDocument() {
+            var config = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+
+            var endpoint = config["CosmosEndpoint"];
+            var masterKey = config["CosmosMasterKey"];
+
+            using (var client = new CosmosClient(endpoint, masterKey))
+            {
+                var container = client.GetContainer("tododb", "todo");
+                var sql = "Select * from c";
+                var iterator = container.GetItemQueryIterator<dynamic>(sql);
+                var page = await iterator.ReadNextAsync();
+
+                foreach (var item in page)
+                {
+                    Console.WriteLine($"Todo { item.id } has { item.childern.count } childern");
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
